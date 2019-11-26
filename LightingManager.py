6f7c114312307d6bee65edb31534a158f5d65ca0
Chase Miller <46936509+ChaseMillers@users.noreply.github.com>
2019-11-26 07:26:31 -0800

    Update LightingManager.py
    Turn lights on and off.

--- LightingManager.py
+++ LightingManager.py
@@ -35,12 +35,43 @@ class LightManager(QtWidgets.QDialog):
         createBtn.clicked.connect(self.createLight)
         layout.addWidget(createBtn, 0, 1)
 
+        scrollWidget = QtWidgets.QWidget()
+        scrollWidget.setSizePolicy(QtWidgets.QSizePolicy.Maximum, QtWidgets.QSizePolicy.Maximum)
+        self.scrollLayout = QtWidgets.QVBoxLayout(scrollWidget)
+
+        scrollArea = QtWidgets.QScrollArea()
+        scrollArea.setWidgetResizable(True)
+        scrollArea.setWidget(scrollWidget)
+        # add to row 1, at colmn 0, take 1 row, and 2 columns
+        layout.addWidget(scrollArea, 1, 0, 1, 2)
+
     def createLight(self):
         lightType = self.lightTypeCB.currentText()
         func = self.lightTypes[lightType]
 
         light = func()
+        widget = LightWidget(light)
+        self.scrollLayout.addWidget(widget)
 
+class LightWidget(QtWidgets.QWidget):
+
+    def __init__(self, light):
+        super(LightWidget, self).__init__()
+        if isinstance(light, basestring):
+            light = pm.PyNode(light)
+
+        self.light = light
+        self.buildUI()
+    
+    def buildUI(self):
+        layout = QtWidgets.QGridLayout(self)
+
+        self.name = QtWidgets.QCheckBox(str(self.light.getTransform()))
+        self.name.setChecked(self.light.visibility.get())
+        # lambda accepts a value then uses it as a variable, basicly its a function 
+        # you only use once because it dosen't have or need a name. "anomas function"
+        self.name.toggled.connect(lambda val: self.light.getTransform().visibility.set(val))
+        layout.addWidget(self.name, 0, 0)
 
 def showUI():
     ui = LightManager()
